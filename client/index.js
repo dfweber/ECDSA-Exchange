52556e6aeaafd1e1c0793967091d566c97664719
Dan Weber <danielfw85@live.com>
2021-05-03 15:07:30 -0700

    ECDSA exchange

--- client/index.js
+++ client/index.js
@@ -1,34 +1,134 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import "./index.scss";
 
-const server = "http://localhost:3042";
+const EC = require("elliptic").ec;
+import SHA256 from "crypto-js/sha256";
 
-document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
-  if(value === "") {
-    document.getElementById("balance").innerHTML = 0;
-    return;
+const App = () => {
+  const [address, setAddress] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [allBalances, setAllBalances] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const [recipient, setRecipient] = useState(null);
+  const [privateKey, setPrivateKey] = useState(null);
+  const ec = new EC("secp256k1");
+
+  function handleTransferAmount() {
+    const transaction = {
+      amount,
+      recipient,
+    };
+
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    const signature = key.sign(SHA256(JSON.stringify(transaction)).toString());
+    const body = JSON.stringify({
+      transaction,
+      signature: signature.toDER("hex"),
+      publicKey: key.getPublic().encode("hex"),
+    });
+
+    const request = new Request(`http://localhost:3042/send`, {
+      method: "POST",
+      body,
+    });
+
+    fetch(request, { headers: { "Content-Type": "application/json" } })
+      .then((response) => {
+        return response.json();
+      })
+      .then(({ balance }) => {
+        setBalance(balance);
+      });
   }
 
-  fetch(`${server}/balance/${value}`).then((response) => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
-});
+  function handleGetBalances() {
+    fetch("http://localhost:3042/balances")
+      .then((response) => {
+        return response.json();
+      })
+      .then(({ balances }) => {
+        setAllBalances(balances);
+      });
+  }
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
-  const amount = document.getElementById("send-amount").value;
-  const recipient = document.getElementById("recipient").value;
+  useEffect(() => {
+    if (address === "" || address?.length < 16) {
+      return null;
+    }
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+    fetch(`http://localhost:3042/balance/${address}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then(({ balance }) => {
+        setBalance(balance);
+      });
+  }, [address]);
 
-  const request = new Request(`${server}/send`, { method: 'POST', body });
+  useEffect(() => {
+    handleGetBalances();
+  }, []);
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
-});
+  return (
+    <div>
+      <div className="components">
+        <div className="wallet">
+          <h1>Your Wallet</h1>
+          <input
+            type="text"
+            id="exchange-address"
+            placeholder="Your Address"
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <div id="balance">{balance}</div>
+        </div>
+
+        <div className="send">
+          <h1>Send Amount</h1>
+          <input
+            type="text"
+            id="private-key"
+            placeholder="Private Key"
+            onChange={(e) => setPrivateKey(e.target.value)}
+          />
+          <input
+            type="text"
+            id="send-amount"
+            placeholder="Send Amount"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <input
+            type="text"
+            id="recipient"
+            placeholder="Recipient"
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+          <div
+            className="button"
+            id="transfer-amount"
+            onClick={() => {
+              handleTransferAmount();
+              handleGetBalances();
+            }}
+          >
+            Transfer Amount
+          </div>
+        </div>
+        <div>
+          {allBalances &&
+            Object.entries(allBalances).map((key) => (
+              <>
+                <p>
+                  <b>address:</b> {key.toString().slice(0, 4)}...
+                  {key[0].slice(-4)} <b>balance:</b> {key[1]}
+                </p>
+              </>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const EC = require("elliptic").ec;
+const SHA256 = require("crypto-js/sha256");
 const port = 3042;
 
 // localhost can have cross origin errors
@@ -8,23 +10,44 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const ec = new EC("secp256k1");
+
+const balances = {};
+for (let i = 0; i < 10; i++) {
+  const key = ec.genKeyPair();
+  const publicKey = key.getPublic().encode("hex");
+  console.log(
+    publicKey +
+      " with a private key of " +
+      key.getPrivate().toString(16) +
+      " has a balance of 100"
+  );
+  balances[publicKey] = 100;
 }
 
-app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+app.get("/balances/", (req, res) => {
+  res.send({ balances });
+});
+
+app.post("/send", (req, res) => {
+  const { signature, transaction, publicKey } = req.body;
+  const key = ec.keyFromPublic(publicKey, "hex");
+  const hash = SHA256(JSON.stringify(transaction)).toString();
+  if (key.verify(hash, signature)) {
+    console.log("-----> ", req.body);
+    balances[publicKey] -= transaction.amount;
+    balances[transaction.recipient] =
+      (balances[transaction.recipient] || 0) + +transaction.amount;
+    res.send({ balance: balances[publicKey] });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 app.listen(port, () => {
